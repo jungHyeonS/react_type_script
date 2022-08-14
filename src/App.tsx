5e3587942f91a745730b64d9ad1f5521c11d7ebf
jungHyeonS <fbtls25@naver.com>
2022-08-14 21:12:47 +0900

    react-master-class : typescript recap

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,27 @@ const H1 = styled.h1`
   color: ${props => props.theme.textColor};
 `;
 
+interface DummyProps{
+  text:string
+  active?:boolean
+}
+
+function Dummy({text,active = false} :DummyProps){
+  return <h1>{text}</h1>
+}
 
 
 function App() {
+  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    
+  }
+
   return (
     <Container>
       <H1>test</H1>
+      <Dummy active={true} text="hello"></Dummy>
+        <button onClick={onClick}>click me</button>
+      
     </Container>
   );
 }
